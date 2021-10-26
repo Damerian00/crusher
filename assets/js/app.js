901cba87750967cbca39bb7abe44c3989ef186b3
Damerian Dark <newbmailz@gmail.com>
2021-10-26 10:09:58 -0500

    have drag and drop working

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,4 +31,46 @@ function makeBoard () {
 }
 makeBoard()
 
+/* dragging images */
+let imgBeingDragged;
+let imgBeingReplaced;
+let divIdbeingDragged;
+let divIdbeingReplaced
+
+squares.forEach(div => div.addEventListener('dragstart', dragStart));
+squares.forEach(div => div.addEventListener('dragend', dragEnd));
+squares.forEach(div => div.addEventListener('dragover', dragOver));
+squares.forEach(div => div.addEventListener('dragleave', dragLeave));
+squares.forEach(div => div.addEventListener('dragenter', dragEnter));
+squares.forEach(div => div.addEventListener('drop', dragDrop));
+
+function dragStart () {
+ imgBeingDragged = this.style.backgroundColor;
+ divIdbeingDragged = parseInt(this.id);
+}
+
+function dragEnd () {
+    
+}
+
+function dragOver (e) {
+   e.preventDefault() 
+}
+
+function dragLeave () {
+    
+}
+
+function dragEnter (e) {
+   e.preventDefault() 
+}
+
+function dragDrop () {
+    imgBeingReplaced = this.style.backgroundColor;
+    divIdbeingReplaced = parseInt(this.id);
+    squares[divIdbeingDragged].style.backgroundColor = imgBeingReplaced
+}
+
+
+
 })
