49979d002ea93e4d8add12560219bb0a248104f3
Damerian Dark <newbmailz@gmail.com>
2021-10-26 15:45:56 -0500

    added check for 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -100,13 +100,12 @@ function moveDown(){
     for (let i = 0; i < width; i++) {
         firstRow.push(i);
     }
-
     for ( i = 0; i < boardWidth-width; i++) {
         if (squares[i + width].style.backgroundImage === ''){
             squares[i + width].style.backgroundImage = squares[i].style.backgroundImage;
             squares[i].style.backgroundImage = '';
             const isFirstRow = firstRow.includes(i);
-            if (isFirstRow && squares[i].style.backgroundImage === ''){
+            if (isFirstRow && (squares[i].style.backgroundImage === '')){
                 let randomImg = Math.floor(Math.random() * crushImgs.length);
                 squares[i].style.backgroundImage = crushImgs[randomImg]
             }
@@ -115,6 +114,57 @@ function moveDown(){
 }
 
 /* match checking */
+
+/* check for 5 */
+
+function checkRowForFive(){
+    let notValid = [];
+    for (let t = 1; t < width; t++) {
+        let a = (width*t) -1;
+        let b = (width*t) -2;
+        let c = (width*t) -3;
+        let d = (width*t) -4;
+        notValid.push(a);
+        notValid.push(b);
+        notValid.push(c);
+        notValid.push(d);
+        
+    } 
+    for ( i = 0; i < boardWidth-2; i++) {
+     let rowOfFive = [i, i+1, i+2, i+3]
+     let decidedImg = squares[i].style.backgroundImage
+     const isBlank =  squares[i].style.backgroundImage === ''
+    if (notValid.includes(i)) continue
+    
+    if (rowOfFive.every(index => squares[index].style.backgroundImage === decidedImg && !isBlank)) {
+        score += 5
+        scoreD.innerHTML = score 
+        rowOfFive.forEach(index => {
+            squares[index].style.backgroundImage = ''
+        })
+    }
+}
+
+}
+function checkColumnForFive(){
+    
+    let fiveWidth = (width*4)+1
+    for ( i = 0; i < boardLength-fiveWidth; i++) {
+     let columnOfFive = [i, i+width, i+width*2, i+width*3]
+     let decidedImg = squares[i].style.backgroundImage
+     const isBlank =  squares[i].style.backgroundImage === ''
+
+     if (columnOfFive.every(index => squares[index].style.backgroundImage === decidedImg && !isBlank)) {
+        score += 5
+        scoreD.innerHTML = score
+        columnOfFive.forEach(index => {
+             squares[index].style.backgroundImage = ''
+         })
+     }
+    }
+
+}
+
 
 /* check for 4 */
 
@@ -152,7 +202,6 @@ function checkColumnForFour(){
      let columnOfFour = [i, i+width, i+width*2, i+width*3]
      let decidedImg = squares[i].style.backgroundImage
      const isBlank =  squares[i].style.backgroundImage === ''
-    //  if (notValid.includes(i)) continue
 
      if (columnOfFour.every(index => squares[index].style.backgroundImage === decidedImg && !isBlank)) {
         score += 4
@@ -212,10 +261,13 @@ function checkColumnForThree(){
 
 window.setInterval(function(){
     moveDown()
+    checkRowForFive()
+    checkColumnForFive()
     checkRowForFour()
     checkColumnForFour()
     checkRowForThree()
     checkColumnForThree()
+    
 }, 100)
 
 })
